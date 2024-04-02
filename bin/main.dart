ce5b3799905acbe4fdde515b3d839da0f56e3698
LG <34185919+projektorius96@users.noreply.github.com>
2024-04-02 18:03:57 +0100

    one way communication to main.log writing charCodes, see desc.
    - if you open browser console you can confirm:
    ```js
    let charCodesFromDart = [123, 34, 116, 101, 120, 116, 34, 58, 34, 72, 101, 108, 108, 111, 34, 125];
    JSON.parse(String.fromCharCode(...charCodesFromDart))
    // {text: 'Hello'}, which is exactly what I have sent from Google Chrome extension to this Native messaging host
    ===
    To recap: not exactly as expected, but with help of community, at least one-way communication-wise, this could work, just need to issue a help request
    ```

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,8 @@
 import 'dart:io' as dart_io;
+
 import 'package:native_messaging_host/src/native_messaging_host_base.dart';
+/* import 'dart:convert' as dart_converter; */
+/* import 'package:native_messaging_host/src/native_messaging_host_base.dart'; */
 
 void main(List<String> arguments) {
   // Define the path to the log file
@@ -12,9 +15,16 @@ void main(List<String> arguments) {
   var sink = logFile.openWrite(mode: dart_io.FileMode.append);
 
   dart_io.stdin.listen((data) {
-    sink.addStream(
-        Stream.fromIterable([data])
-    );
+    /* IGNORE */
+      /* sink.addStream(
+              Stream.fromIterable([data])
+      ); */
+      /* logFile.writeAsString(String.fromCharCodes(decodeMessage(data).toString().codeUnits)); */// # [123, 34, 116, 101, 120, 116, 34, 58, 34, 72, 101, 108, 108, 111, 34, 125]
+    /* IGNORE */
+
+    // DEV_NOTE # somewhy String.fromCharCodes forces Chrome to exit native messaging host with Error: Native host has exited.
+    sink.write( /* String.fromCharCodes( */ decodeMessage(data) /* ) */ ) ; /* [123, 34, 116, 101, 120, 116, 34, 58, 34, 72, 101, 108, 108, 111, 34, 125] */// yet in JavaScript String.fromCharCodes([123, 34, 116, 101, 120, 116, 34, 58, 34, 72, 101, 108, 108, 111, 34, 125]) returns expected "{text: 'Hello'}"
+    /* dart_io.stdout.write(encodeMessage( String.fromCharCodes( [123, 34, 116, 101, 120, 116, 34, 58, 34, 72, 101, 108, 108, 111, 34, 125] ) )); */// exits Native messaging host with Error: Native host has exited.
   });
 
   // Graceful termination on Ctrl+C (SIGINT), see [cont'd] below
