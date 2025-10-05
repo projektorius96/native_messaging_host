b38e319ec7ad7f03ec8c25938665f0b8ef6524cc
LG <34185919+projektorius96@users.noreply.github.com>
2025-10-05 14:30:30 +0300

    code revision 1.1: patch for [commit:4d12235]

--- lib/src/native_messaging_host_base.dart
+++ lib/src/native_messaging_host_base.dart
@@ -1,6 +1,6 @@
 import 'dart:io' as dart_io;
 import 'dart:convert' as dart_convert;
-import 'dart:typed_data' as dart_buffer;
+import 'dart:typed_data' as typed_data;
 
 /// The `captureStdin` will capture stream and write to file as String 
 dart_io.IOSink captureStdin(List<int> charCodesInt, {String outdir = 'log.jsonl'}){
@@ -12,41 +12,48 @@ dart_io.IOSink captureStdin(List<int> charCodesInt, {String outdir = 'log.jsonl'
 }
 
 /// Native Messaging Host encoding implementation, hence `encodeMessage`
-encodeMessage(jsonString) {
-  /// DEV_NOTE # Encode the JSON string to UTF-8
-  final encodedJson = dart_convert.jsonEncode(jsonString);
+encodeMessage(data) {
+  
+  // Encodes the [data] as UTF-8 represented by `Uint8List` view. 
+  final typed_data.Uint8List encodedJson = dart_convert.utf8.encode(data);
 
-  /// DEV_NOTE # Calculate the message length
+  // DEV_NOTE # Calculate the message length
   final messageLength = encodedJson.length;
 
-  /// DEV_NOTE # Allocate a buffer for the entire message (including the 32-bit prefix)
-  final buffer = dart_buffer.Uint8List(4 + messageLength);
+  /// DEV_NOTE # Hard-coded offset equivalent to the length of the message
+  final offset = 4;
 
-  /// DEV_NOTE # Write the message length as a 32-bit little endian integer
+  // DEV_NOTE # Allocate a buffer for the entire message (including the 32-bit prefix)
+  final buffer = typed_data.Uint8List(offset + messageLength);
+
+  // DEV_NOTE # Write the message length as a 32-bit little endian integer
   buffer.buffer
       .asByteData()
-      .setInt32(0, messageLength, dart_buffer.Endian.little);
+      .setInt32(0, messageLength, typed_data.Endian.little);
 
-  /// DEV_NOTE # Copy the encoded JSON into the buffer starting from the offset
-  buffer.setAll(4, encodedJson.codeUnits);
+  // DEV_NOTE # Copy the encoded JSON into the buffer starting from the offset
+  buffer.setAll(offset, encodedJson.toString().codeUnits);
 
   return buffer;
+
 }
 
 /// Native Messaging Host decoding implementation, hence `decodeMessage`
-decodeMessage(buffer) {
+decodeMessage(data) {
+
   /// DEV_NOTE # Read message length via getInt32 view
   final messageLength =
-      buffer.buffer.asByteData().getInt32(0, dart_buffer.Endian.host);
+      data.buffer.asByteData().getInt32(0, typed_data.Endian.host);
 
-  /// DEV_NOTE # Hard-coded offset
+  /// DEV_NOTE # Hard-coded offset equivalent to the length of the message
   final offset = 4;
 
   /// DEV_NOTE # Get the payload without the 32-bit prefix
-  final payload = buffer.sublist(offset, messageLength + offset);
+  final payload = data.sublist(offset, messageLength + offset);
 
   /// DEV_NOTE # Decode the payload from UTF-8 to a String
   final jsonString = dart_convert.jsonDecode(payload.toString());
 
   return jsonString;
+
 }
